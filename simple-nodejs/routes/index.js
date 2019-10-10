1860863400b562f63c98b04c9689dda4f18d313f
hryer <harryermawan@gmail.com>
2019-10-10 13:35:29 +0700

    mend

--- simple-nodejs/routes/index.js
+++ simple-nodejs/routes/index.js
@@ -8,6 +8,7 @@ var fb_config = require('./fb_config');
 const me_endpoint_base_url = 'https://graph.accountkit.com/' + fb_config.account_kit_api_version + '/me';
 const token_exchange_base_url = 'https://graph.accountkit.com/' + fb_config.account_kit_api_version + '/access_token';
 var csrf_guid = guid.raw();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -49,8 +50,36 @@ router.post('/login_success', function (req, res) {
   else {
     // login failed
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end("Something went wrong. :( ");
+    res.end("Something went wrong. : ");
   }
 });
 
+router.post('/logout', function (req,res) {
+  console.log('logout');
+  // var logout_url = 'https://graph.accountkit.com/v1.3/' + req.body.account_id + '/invalidate_all_tokens?access_token=AA|'+ fb_config.app_id + '|' + fb_config.app_secret;
+  // var logout_url = 'https://graph.accountkit.com/v1.3/logout?access_token=' +   req.body.access_token;
+  var logout_url = 'https://graph.accountkit.com/v1.3/' + req.body.account_id + '?access_token=AA|' + fb_config.app_id + '|' + fb_config.app_secret;
+  console.log(logout_url);
+  
+  let config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // axios.post(logout_url,{},config)
+  //   .then(function (response) {
+  //     res.render('index', { title: 'Express', appId: fb_config.app_id, csrf: csrf_guid, version: fb_config.account_kit_api_version });
+  //   }).catch(function (error) {
+  //     res.end('fail');
+  //   });https://graph.accountkit.com/v1.3/<account_id>?access_token=AA|<facebook_app_id>|<app_secret>
+
+  axios.delete(logout_url,config)
+  .then(function (response) {
+    res.render('index', { title: 'hehe', appId: fb_config.app_id, csrf: csrf_guid, version: fb_config.account_kit_api_version });
+  }).catch(function (error) {
+    res.end('fail');
+  });
+
+})
 module.exports = router;
